8114050613e5961a61e6d70792471a221b0f2584
Jordi Irawan <jordirwn@gmail.com>
2023-01-28 21:30:54 +0700

    up

--- src/Applications/use_case/_test/GetThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadUseCase.test.js
@@ -4,24 +4,50 @@ const GetThreadUseCase = require('../GetThreadUseCase');
 
 describe('GetThreadUseCase', () => {
   it('should get return detail thread correctly', async () => {
+    const fakeThreadId = 'thread-1';
     const useCasePayload = {
-      threadId: 'thread-1',
+      threadId: fakeThreadId,
     };
-
+    const fakeThread = {
+      id: fakeThreadId,
+      title: 'title',
+      body: 'body',
+      date: new Date().toISOString(),
+      username: 'wan',
+    };
+    const fakeComments = [
+      {
+        id: 'comment-1',
+        username: 'wan1',
+        date: new Date().toISOString(),
+        content: 'content',
+      },
+      {
+        id: 'comment-2',
+        username: 'wan2',
+        date: new Date().toISOString(),
+        content: '**komentar telah dihapus**',
+      },
+    ];
     const mockThreadRepository = new ThreadRepository();
     const mockCommentRepository = new CommentRepository();
 
-    mockThreadRepository.existsThread = jest.fn().mockImplementation(() => Promise.resolve());
-    mockThreadRepository.getThread = jest.fn().mockImplementation(() => Promise.resolve());
-    mockCommentRepository.getCommentsThread = jest.fn().mockImplementation(() => Promise.resolve());
+    mockThreadRepository.existsThread = jest.fn().mockImplementation(() => Promise.resolve(true));
+    mockThreadRepository.getThread = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(fakeThread));
+    mockCommentRepository.getCommentsThread = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(fakeComments));
 
     const threadUseCase = new GetThreadUseCase({
       threadRepository: mockThreadRepository,
       commentRepository: mockCommentRepository,
     });
 
-    await threadUseCase.execute(useCasePayload);
+    const thread = await threadUseCase.execute(useCasePayload);
     expect(mockThreadRepository.getThread).toHaveBeenCalledWith(useCasePayload.threadId);
     expect(mockCommentRepository.getCommentsThread).toHaveBeenCalledWith(useCasePayload.threadId);
+    expect(thread).toStrictEqual({ ...fakeThread, comments: fakeComments });
   });
 });
